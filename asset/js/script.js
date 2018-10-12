200b5f7c4525151e6abef06e98ce0b8cd3788204
diegoveega <00148816@uca.edu.sv>
2018-10-12 02:24:25 -0600

    commit ava

---
+++ asset/js/script.js
@@ -0,0 +1,15 @@
+var container = document.getElementById("c1");
+container.innerText='Hola Mundo';
+container.style.backgroundColor= '#000000';
+container.style.color = '#1cb723'
+
+container.style.width= '200px';
+container.style.height= '200px';
+
+container.innerHTML= "<input type='text' placeholder='EScriba sdu texto'>"
+
+var containers=document.getElementsByClassName('c2');
+
+containers.forEach(element => {
+    element.style.color= '#009999';
+});
